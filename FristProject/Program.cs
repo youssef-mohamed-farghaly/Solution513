2d0ff5c14372e8e5660a0361f7f930587be31068
كمبيولاب <@DESKTOP-V2DT3JS>
2024-12-30 22:27:01 +0200

    Add project files.

---
+++ FristProject/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace FristProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your favorite number : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"No really ,{num} is my favorite number");
+        }
+    }
+}
---
+++ Task/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int PriceSmall = 25;
+            int PriceBig = 30;
+            Console.WriteLine("Hello for currnet clean");
+            Console.Write("Enter number of small currnet : ");
+            int smallCurnet = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter number of big currnet : ");
+            int bigCurnet = Convert.ToInt32(Console.ReadLine());
+            int Cost = (smallCurnet * PriceSmall) + (bigCurnet * PriceBig);
+            double Tax = Cost * .06 ;
+            double Total = Cost + Tax;
+            Console.WriteLine($"The cost is : {Cost}");
+            Console.WriteLine($"The tax is : {Tax}");
+            Console.WriteLine($"Total is : {Total}");
+            Console.WriteLine("Thanks for uses program.");
+
+        }
+    }
+}
